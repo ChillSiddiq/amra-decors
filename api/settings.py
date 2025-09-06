3b4874b2c61ae7d5bb7f78a73b72c8944b0229e9
Mohamed Siddiq <siddiq@cyberheals.com>
2025-09-06 16:38:19 +0530

    Add: Vercel Speed Insights

--- api/settings.py
+++ api/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'api.middleware.VercelSpeedInsightsMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
